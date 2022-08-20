94af29638d06a079c176043f3f56e2c2917aa436
josoomin <99530921+josoomin@users.noreply.github.com>
2022-08-20 17:48:00 +0900

    22.08.20

--- Assets/Scripts/CameraRotator.cs
+++ Assets/Scripts/CameraRotator.cs
@@ -9,20 +9,12 @@ namespace roomescape
         public CameraRotator _main;
 
         public float _angle = 90.0f;
-        public float _rec = 45.0f;
-        public float _move = 100.0f;
 
         public GameObject _ItemMenu;
 
-        public GameObject _upB;
-        public GameObject _downB;
-        public GameObject _leftB;
-        public GameObject _rightB;
-        public GameObject _ItemB;
-
         void Start()
         {
-            _upB.SetActive(false);
+
         }
 
         void Update()
@@ -30,54 +22,16 @@ namespace roomescape
 
         }
 
-
-        // > ¿À¸¥ÂÊ ¹öÆ° ´©¸£¸é, Ä«¸Þ¶ó °¢µµ(yÃà) 90¾¿ Áõ°¡
+        // > ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Æ° ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½, Ä«ï¿½Þ¶ï¿½ ï¿½ï¿½ï¿½ï¿½(yï¿½ï¿½) 90ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
         public void OnClickRightButton()
         {
             _main.transform.Rotate(new Vector3(0, _angle, 0));
         }
 
-        // < ¿ÞÂÊ ¹öÆ° ´©¸£¸é, Ä«¸Þ¶ó °¢µµ(yÃà) 90¾¿ °¨¼Ò
+        // < ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Æ° ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½, Ä«ï¿½Þ¶ï¿½ ï¿½ï¿½ï¿½ï¿½(yï¿½ï¿½) 90ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
         public void OnClickLeftButton()
         {
             _main.transform.Rotate(new Vector3(0, -_angle, 0));
-        }
-
-        // À§ ¹öÆ° ´©¸£¸é Ä«¸Þ¶ó °¢µµ (xÃà) 45 Áõ°¡
-        public void OnClickUpButton()
-        {
-            _main.transform.Rotate(new Vector3(-_rec, 0, 0));
-            _upB.SetActive(false);
-            _downB.SetActive(true);
-            _leftB.SetActive(true);
-            _rightB.SetActive(true);
-        }
-
-        // ¾Æ·¡ ¹öÆ° ´©¸£¸é Ä«¸Þ¶ó °¢µµ (xÃà) 45 °¨¼Ò
-        public void OnClickDownButton()
-        {
-            _main.transform.Rotate(new Vector3(_rec, 0, 0));
-            _upB.SetActive(true);
-            _downB.SetActive(false);
-            _leftB.SetActive(false);
-            _rightB.SetActive(false);
-        }
-
-        public void OnClickUpItemMenu()
-        {
-            if (_downB.activeSelf == true)
-            {
-                _downB.SetActive(false);
-                _ItemMenu.transform.position = new Vector3(0, _move, 0);
-            }
-
-            else if (_downB.activeSelf == false)
-            {
-                _downB.SetActive(true);
-                _ItemMenu.transform.position = new Vector3(0, -_move, 0);
-            }
-
-            
         }
     }
 }
